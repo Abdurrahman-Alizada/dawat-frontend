8dcdc5aeef10ebc6b144f92d521e6d65f1c800f2
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2023-03-16 18:59:12 +0500

    server changed

--- src/redux/axios.js
+++ src/redux/axios.js
@@ -4,5 +4,5 @@ export const instance = axios.create({
     baseURL: 'https://dawat-backend.onrender.com/',
   });
   
-// export const baseURL = 'https://dawat-backend.onrender.com'
-export const baseURL = 'https://a6c5-111-119-178-165.au.ngrok.io'
+export const baseURL = 'https://dawat-backend.onrender.com'
+// export const baseURL = 'https://a6c5-111-119-178-165.au.ngrok.io'
