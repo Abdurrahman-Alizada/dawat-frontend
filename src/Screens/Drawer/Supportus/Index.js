b74f1273df1e79a074cac1b7b8d3df62ada18bce
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2023-05-07 17:06:27 +0500

    ads page added

--- src/Screens/Drawer/Supportus/Index.js
+++ src/Screens/Drawer/Supportus/Index.js
@@ -1,17 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {Button, ActivityIndicator, View} from 'react-native';
+import {ActivityIndicator, View} from 'react-native';
 import {
   InterstitialAd,
+  RewardedInterstitialAd,
+  RewardedAd,
+  RewardedAdEventType,
   AdEventType,
   TestIds,
 } from 'react-native-google-mobile-ads';
-import {Appbar} from 'react-native-paper';
+import {Appbar, Button, Text, Card} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
-const adUnitId = __DEV__
+const adUnitIdINTERSTITIAL = __DEV__
   ? TestIds.INTERSTITIAL
   : 'ca-app-pub-9526618780177325/2035594601';
 
-const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+const adUnitIdREWARDED = __DEV__
+  ? TestIds.REWARDED
+  : 'ca-app-pub-9526618780177325/7227206913';
+
+const interstitial = InterstitialAd.createForAdRequest(adUnitIdINTERSTITIAL, {
+  requestNonPersonalizedAdsOnly: true,
+});
+
+const rewarded = RewardedAd.createForAdRequest(adUnitIdREWARDED, {
   requestNonPersonalizedAdsOnly: true,
 });
 
@@ -19,6 +30,7 @@ export default function App() {
   const navigate = useNavigation();
 
   const [loaded, setLoaded] = useState(false);
+  const [isInterstitialWatched, setIsInterstitialWatched] = useState(false);
 
   useEffect(() => {
     const unsubscribe = interstitial.addAdEventListener(
@@ -30,35 +42,107 @@ export default function App() {
 
     // Start loading the interstitial straight away
     interstitial.load();
-
     // Unsubscribe from events on unmount
     return unsubscribe;
   }, []);
 
+  const [loaded1, setLoaded1] = useState(false);
+  const [isrewardedWatched, setIsrewardedWatched] = useState(false);
+
+  useEffect(() => {
+    const unsubscribeLoaded = rewarded.addAdEventListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        setLoaded1(true);
+      },
+    );
+    const unsubscribeEarned = rewarded.addAdEventListener(
+      RewardedAdEventType.EARNED_REWARD,
+      reward => {
+        console.log('User earned reward of ', reward);
+      },
+    );
+
+    // Start loading the rewarded ad straight away
+    rewarded.load();
+    // Unsubscribe from events on unmount
+    return () => {
+      unsubscribeLoaded();
+      unsubscribeEarned();
+    };
+  }, []);
 
   return (
-    <View style={{flex:1}}>
+    <View style={{flex: 1}}>
       <Appbar.Header>
         <Appbar.BackAction onPress={() => navigate.goBack()} />
         <Appbar.Content title="Watch an ad" />
-        {/* <Appbar.Action icon="calendar" onPress={() => {}} /> */}
-        {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
       </Appbar.Header>
-      <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
 
-      {loaded ? (
-        <Button
-          title="Show ad"
-          onPress={() => {
-            interstitial.show();
-          }}
-        />
-      ) : (
-        <ActivityIndicator
-          size="large"
-          style={{}}
-        />
-      )}
+      <Card style={{marginVertical: '0%'}}>
+        <Card.Content>
+          <Text variant="headlineMedium">Thank you for coming here.</Text>
+          <Text style={{marginVertical:"5%"}} variant="bodyLarge">
+            We greatly appreciate your support by watching an Ad. Your
+            support helps us continue to maintain, improve and enhance our app for you and
+            all of our users.
+          </Text>
+        </Card.Content>
+      </Card>
+
+      <View
+        style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start'}}>
+        {!isInterstitialWatched ? (
+          <View style={{width: '90%', marginVertical: '5%'}}>
+            {loaded ? (
+              <Button
+                icon="image-filter-vintage"
+                mode="contained"
+                onPress={() => {
+                  interstitial.show();
+                  setIsInterstitialWatched(true);
+                }}
+                style={{paddingVertical: '1%'}}>
+                Watch Image Ad
+              </Button>
+            ) : (
+              <ActivityIndicator size="large" style={{}} />
+            )}
+          </View>
+        ) : (
+          <Text style={{marginVertical: '5%'}}>
+            You have watched the ad
+          </Text>
+        )}
+
+        {!isrewardedWatched ? (
+          <View style={{width: '90%'}}>
+            {loaded1 ? (
+              <Button
+                icon="motion-play"
+                mode="outlined"
+                onPress={() => {
+                  rewarded.show();
+                  setIsrewardedWatched(true);
+                }}
+                style={{paddingVertical: '1%'}}>
+                Watch Rewarded Ad
+              </Button>
+            ) : (
+              <ActivityIndicator size="large" style={{}} />
+            )}
+          </View>
+        ) : (
+          <Text>You have watched the ad</Text>
+        )}
+
+       
+       {
+         isInterstitialWatched && isrewardedWatched &&
+        <Text style={{marginVertical: '10%'}}>
+          You can watch ad again when you re-open the app.
+        </Text>
+       }
       </View>
     </View>
   );
